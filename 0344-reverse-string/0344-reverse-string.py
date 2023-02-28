57b533eb10c7938f9101bc595681f0605d5720e9
nathy-min <69003813+nathy-min@users.noreply.github.com>
2023-02-28 17:51:43 +0300

    Time: 216 ms (27.68%), Space: 42.4 MB (5.49%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,12 +1,22 @@
+
 class Solution:
+    def reverser(self,s, ptr):
+        if ptr  == len(s)//2:
+            # s[ptr], s[~ptr] = s[~ptr], s[ptr]
+            # print(s[ptr])
+            # print(s[~ptr])
+            return
+        s[ptr], s[~ptr] = s[~ptr], s[ptr]
+        self.reverser(s, ptr +  1)
+        return
+        
+    
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        if len(s) == 1:
-            return
-        val = s.pop()
-        self.reverseString(s)
-        s.insert(0, val)
-        return 
+        ptr = 0
+        print(len(s))
+
+        self.reverser(s,ptr)
         
