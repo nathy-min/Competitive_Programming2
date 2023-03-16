2201b4525b87c0ac48c3d05cee7ae2d827b5cf49
nathy-min <69003813+nathy-min@users.noreply.github.com>
2023-03-16 11:45:36 +0300

    Time: 77 ms (29.49%), Space: 13.7 MB (99.75%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,19 +1,15 @@
 class Solution:
     def combination(self, visited, idx):
-        if idx == len(self.candidates):
-            return
         if sum(visited) == self.target:
             self.ans.append(list(visited))
             return
         if sum(visited) > self.target:
             return
         
-        
-        visited.append(self.candidates[idx])
-        self.combination(visited, idx)
-        
-        visited.pop()
-        self.combination(visited, idx + 1)
+        for i in range(idx, len(self.candidates)):
+            visited.append(self.candidates[i])
+            self.combination(visited, i)
+            visited.pop()
         
            
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
