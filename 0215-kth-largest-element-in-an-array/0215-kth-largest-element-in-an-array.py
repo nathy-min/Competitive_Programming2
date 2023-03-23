ff6e0f1548921bc6b368654dd5dbf984c6edaeb6
nathy-min <69003813+nathy-min@users.noreply.github.com>
2023-03-23 19:43:08 +0300

    Time: 1456 ms (20.15%), Space: 30.1 MB (25.38%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,5 +1,30 @@
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort(reverse = True)
+    def quick_sort(self , left , right , arr , k):
+        # if k == :
+        #     return arr[k]
+
+        write = left + 1
+        read = left + 1
+        while read <= right:
+            if arr[read] < arr[left]:
+                arr[read] , arr[write] = arr[write] , arr[read]
+                write += 1
+
+            read += 1
+
+        arr[write - 1] , arr[left] = arr[left] , arr[write - 1]
+        if write - 1 == k:
+            return arr[write - 1]
         
-        return nums[k - 1]
+        elif k < write - 1:
+            
+            return self.quick_sort(left , write - 2 , arr , k)
+        else:
+            
+            return self.quick_sort(write , right , arr , k)
+        
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return self.quick_sort(0 , len(nums) - 1 , nums ,len(nums) - k )
+        
+        
+        
