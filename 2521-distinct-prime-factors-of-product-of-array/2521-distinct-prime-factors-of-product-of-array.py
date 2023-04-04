ec12cce4d421f44ad964ebc4e8f4fa465f9b9a86
nathy-min <69003813+nathy-min@users.noreply.github.com>
2023-04-04 19:52:15 +0300

    Time: 1530 ms (21.33%), Space: 15.2 MB (91.08%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.py
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.py
@@ -1,40 +1,14 @@
 class Solution:
-    def nextPrime(self, n):
-        is_prime: list[bool] = [True for _ in range(n + 1)]
-        is_prime[0] = is_prime[1] = False
-        primes = []
-        
-        i = 2
-
-        while i * i <= n:
-            if is_prime[i]:
-                j = i * i
-                while j <= n:
-                    is_prime[j] = False
-                    j += i
-            i += 1
-
-        for val in range(len(is_prime)):
-            if is_prime[val]:
-                primes.append(val)
-        
-        return primes
-    def distinctPrimeFactors(self, nums: List[int]) -> int:
-        
-        pdt = prod(nums)
-        d = 0
-        prime_factors = set()
-        
-        primes = self.nextPrime(1000)
-        print(primes)
-        while d < len(primes) :
-            if pdt % primes[d] == 0:
-                prime_factors.add(primes[d])
-                pdt //= primes[d]
+    def prime_factor(self , val):
+        d = 2
+        while val > 1:
+            if val % d == 0:
+                self.sets.add(d)
+                while val % d == 0:
+                    val //= d
             d += 1
-        
-        
-        
-        return len(prime_factors)
-    
-    
+    def distinctPrimeFactors(self, nums: List[int]) -> int:
+        self.sets = set()
+        for num in nums:
+            self.prime_factor(num)
+        return len(self.sets)
