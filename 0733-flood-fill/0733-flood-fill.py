469156acb586e14abd6e1840cf3c4aec9b6d0e6e
nathy-min <69003813+nathy-min@users.noreply.github.com>
2023-04-10 19:34:47 +0300

    Time: 75 ms (77.02%), Space: 13.8 MB (98.66%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,30 @@
+class Solution:
+    def inbound(self, r, c):
+        if r < 0 or r >= len(self.image) or c < 0 or c >= len(self.image[0]):
+            return False
+        return True
+    
+    def dfs(self, r, c):
+        if not self.inbound(r, c):
+            return
+        if (r, c) in self.visited:
+            return
+        
+        if self.image[r][c] == self.start:
+            self.visited.add((r, c))
+            self.image[r][c] = self.color
+            self.dfs(r + 1, c)
+            self.dfs(r - 1, c)
+            self.dfs(r, c + 1)
+            self.dfs(r, c - 1)
+            
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        self.image = image
+        self.start = image[sr][sc]
+        self.color = color
+        self.visited = set()
+        
+        self.dfs(sr, sc)
+        
+        return self.image
+        
