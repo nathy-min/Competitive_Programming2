d45ca2101006ba9b36ad5df8ea2e7150a1988f9e
nathy-min <69003813+nathy-min@users.noreply.github.com>
2023-05-26 11:17:26 +0300

    added increasing-triplet-subsequence.python

---
+++ increasing-triplet-subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = float('inf')
+        second = float('inf')
+        for n in nums:
+            if n > second:
+                print(first, second)
+                return True
+
+            if n > first:
+                second = min(n, second)
+
+            else:
+                first = min(n, first)
+        
+        return False
