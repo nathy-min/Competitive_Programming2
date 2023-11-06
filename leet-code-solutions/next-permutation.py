c567c4e0f19c9425764ffc0fca8ec3e69957a5e7
nathy-min <69003813+nathy-min@users.noreply.github.com>
2023-11-06 22:14:47 +0300

    Added solution for Next Permutation

--- leet-code-solutions/next-permutation.py
+++ leet-code-solutions/next-permutation.py
@@ -1,18 +1,36 @@
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        a = -1
-        b = 0
-        for i in range(len(nums) - 1, -1, -1):
-            for j in range(i - 1, -1, -1):
-                if nums[j] < nums[i]:
-                    a, b = (j, i) if j > a else (a, b)
 
-        if a != -1:
-            nums[a], nums[b] = nums[b], nums[a]
-            nums[a + 1 : ] = sorted(nums[a + 1 :])           
-        else:
-            nums.sort()
-        print(a, b)                
+    def nextPermutation(self, nums: List[int]) -> None:
+
+        def reverse(nums, start):
+
+            end = len(nums) - 1
+
+            while start < end:
+
+                nums[start], nums[end] = nums[end], nums[start]
+
+                start += 1
+
+                end -= 1
+
+        i = len(nums) - 2
+
+        while i >= 0 and nums[i + 1] <= nums[i]:
+
+            i -= 1
+
+        if i >= 0:
+
+            j = len(nums) - 1
+
+            while nums[j] <= nums[i]:
+
+                j -= 1
+
+            nums[i], nums[j] = nums[j], nums[i]
+
+        reverse(nums, i + 1)
+    
+    
+        
